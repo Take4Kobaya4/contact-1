7cfaf26807ae92a5ff5cb7cbc1600f5c4dee05bb
Take4Kobaya4 <t.kobayashi3927@gmail.com>
2022-10-11 14:26:54 +0900

    Setting Route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,31 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $html = "
+    <h1>Contact App</h1>
+    <div>
+        <a href='" . route('contacts.index') . "'>All Contacts</a>
+        <a href='" . route('contacts.create') . "'>Add contact</a>
+        <a href='" . route('contacts.show', 1) . "'>Show contact</a>
+    </div>
+    ";
+    return $html;
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/admin/contacts', function () {
+        return "<h1>All Contacts</h1>";
+    })->name('contacts.index');
+
+    Route::get('/admin/contacts/create', function () {
+        return "<h1>Add new contact</h1>";
+    })->name('contacts.create');
+
+    Route::get('/admin/contacts/{id}', function ($id) {
+        return "Contact" . $id;
+    })->name('contacts.show');
+
+    Route::fallback(function () {
+        return "<h1>ページは存在していません！</h1>";
+    });
 });
