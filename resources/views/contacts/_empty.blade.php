b97de52a5578bedc27e12a1738dc11abba6c3543
Take4Kobaya4 <t.kobayashi3927@gmail.com>
2022-11-01 16:13:01 +0900

    connertcing database

---
+++ resources/views/contacts/_empty.blade.php
@@ -0,0 +1,9 @@
+
+<tr>
+  <td colspan="6">
+    <div class="alert alert-waring">
+      No Contact found.
+    </div>
+  </td>
+</tr>
+
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ContactController;
+
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\CompanyController;
+
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\ActivityController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,32 +21,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $html = "
-    <h1>Contact App</h1>
-    <div>
-        <a href='" . route('contacts.index') . "'>All Contacts</a>
-        <a href='" . route('contacts.create') . "'>Add contact</a>
-        <a href='" . route('contacts.show', 1) . "'>Show contact</a>
-    </div>
-    ";
-    return $html;
-});
 
-Route::prefix('admin')->group(function () {
-    Route::get('/admin/contacts', function () {
-        return "<h1>All Contacts</h1>";
-    })->name('contacts.index');
+Route::get('/', [WelcomeController::class, 'home']);
 
-    Route::get('/admin/contacts/create', function () {
-        return "<h1>Add new contact</h1>";
-    })->name('contacts.create');
 
-    Route::get('/admin/contacts/{id}', function ($id) {
-        return "Contact" . $id;
-    })->name('contacts.show');
+Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
 
-    Route::fallback(function () {
-        return "<h1>ページは存在していません！</h1>";
-    });
-});
+Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
+
+Route::get('/contacts/{id}', [ContactController::class, 'show'])->name('contacts.show');
+
+Route::resource('/companies', CompanyController::class);
+
+Route::resources([
+    '/tags' => TagController::class,
+    '/tasks' => TaskController::class
+]);
+
+Route::resource('/activities', ActivityController::class)->except([
+    'index', 'show'
+]);
+Route::resource('/contacts.notes', ContactController::class)->shallow();
+// Route::resource('/contacts.notes', ContactNoteController::class)->names([
+//     'index' => 'activities.all',
+//     'show' => 'activities.view'
+// ]);
+
+
+Route::resource('/contacts.notes', ContactNoteController::class)->names([
+    'activities' => 'active'
+]);
